9bf1961b979ce59144cbef82f37395006b1331f3
filipdufka-ryzen-pc <filipdufka@gmail.com>
2021-11-17 19:34:42 +0100

    1.0.1 - Formating cleanup

--- Runtime/KiwiProjectile.cs
+++ Runtime/KiwiProjectile.cs
@@ -2,59 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KiwiProjectile : MonoBehaviour {
-    Vector3 origin;
-    Vector3 direction;
-    float position;
-    public float speed = 100.0f;
-    float life = 3.0f; //in seconds
-    public float precision = 0.9f;
-    float shotDelta { get { return Time.fixedDeltaTime * speed; } }
-    public float spawnOffset = 0.1f;
+namespace FruitBowl {
+	public class KiwiProjectile : MonoBehaviour {
+		Vector3 origin;
+		Vector3 direction;
+		float position;
+		public float speed = 100.0f;
+		float life = 3.0f; //in seconds
+		public float precision = 0.9f;
+		float shotDelta { get { return Time.fixedDeltaTime * speed; } }
+		public float spawnOffset = 0.1f;
 
-    public static KiwiProjectile CreateProjectile(Vector3 origin, Vector3 direction) {
-        GameObject go = new GameObject("Projectile");
-        KiwiProjectile kp = go.AddComponent<KiwiProjectile>();
-        kp.SetShot(origin, direction);
-        return kp;
-    }
+		public static KiwiProjectile CreateProjectile(Vector3 origin, Vector3 direction) {
+			GameObject go = new GameObject("Projectile");
+			KiwiProjectile kp = go.AddComponent<KiwiProjectile>();
+			kp.SetShot(origin, direction);
+			return kp;
+		}
 
-    private void Start() {
-    }
+		private void Start() {
+		}
 
-    public void SetShot(Vector3 origin, Vector3 direction) {        
-        this.direction = (direction + Random.insideUnitSphere * (1-precision)).normalized;
-        this.origin = origin + direction * spawnOffset;
-    }
+		public void SetShot(Vector3 origin, Vector3 direction) {
+			this.direction = (direction + Random.insideUnitSphere * (1 - precision)).normalized;
+			this.origin = origin + direction * spawnOffset;
+		}
 
-    void MoveShot() {
-        // Position
-        Vector3 bullet = Vector3.LerpUnclamped(origin, origin + direction, position);
-        transform.position = bullet; // TODO: Make bouncable
-        RaycastHit hit;
-        if(Physics.Raycast(bullet, direction, out hit, shotDelta)) {
-            position = Vector3.Distance(hit.point, origin);
-            Debug.Log(position);
-            // TODO: Check hit layer
-            //Destroy(gameObject);
-            
-            //ILivingThing livingThing;
-            //if((livingThing = hit.collider.GetComponentInParent<ILivingThing>()) != null) {
-            //    livingThing.GotHit();
-            //}
+		void MoveShot() {
+			// Position
+			Vector3 bullet = Vector3.LerpUnclamped(origin, origin + direction, position);
+			transform.position = bullet; // TODO: Make bouncable
+			RaycastHit hit;
+			if (Physics.Raycast(bullet, direction, out hit, shotDelta)) {
+				position = Vector3.Distance(hit.point, origin);
+				Debug.Log(position);
+				// TODO: Check hit layer
+				//Destroy(gameObject);
 
-        } else {
-            position += shotDelta;
-        }        
+				//ILivingThing livingThing;
+				//if((livingThing = hit.collider.GetComponentInParent<ILivingThing>()) != null) {
+				//    livingThing.GotHit();
+				//}
 
-        // Time
-        life -= Time.fixedDeltaTime;
-        if(life < 0) {
-            Destroy(gameObject);
-        }
-    }    
+			} else {
+				position += shotDelta;
+			}
 
-    private void FixedUpdate() {
-        MoveShot();
-    }
+			// Time
+			life -= Time.fixedDeltaTime;
+			if (life < 0) {
+				Destroy(gameObject);
+			}
+		}
+
+		private void FixedUpdate() {
+			MoveShot();
+		}
+	}
 }
